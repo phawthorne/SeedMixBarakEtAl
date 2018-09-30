3c6d7508022a4a16c3cb57efe91caf861eb3c351
Peter Hawthorne <hawt0010@umn.edu>
2018-09-30 15:44:48 -0500

    functioning model code

--- src/SeedMix.jl
+++ src/SeedMix.jl
@@ -1,8 +1,8 @@
 module SeedMix
 
 include("mixes.jl")
-export SpeciesData, MixData
-
-greet() = print("Hello World!")
+export SpeciesData, MixData, length,
+    get_phylo_dist, get_shannon, get_cost, get_consval, get_bloom,
+    genome_to_mix, evaluate, random_mix
 
 end # module
--- src/mixes.jl
+++ src/mixes.jl
@@ -1,12 +1,15 @@
 using CSV
 using DataFrames
 using Parameters
+using Random
+import Base.length
 
+#= Species Data =#
 @with_kw struct SpeciesData
     specpool::Vector{String}
     cost::Vector{Float64}
     conservatism::Vector{Float64}
-    grass::Vector{Float64}
+    grass::Vector{Bool}
     bloom::Array{Float64, 2}
     phylo::Array{Float64, 2}
 end
@@ -25,7 +28,7 @@ function SpeciesData(tablefile::String, phylofile::String)
 
     cost = [table[tablelookup[s], :OneLbCost] for s in specpool]
     conservatism = [table[tablelookup[s], :Conservatism] for s in specpool]
-    grass = [table[tablelookup[s], :Grass_Forb]=="G" ? 1.0 : 0.0 for s in specpool]
+    grass = [table[tablelookup[s], :Grass_Forb]=="G" for s in specpool]
 
     months = [:Apr, :May, :Jun, :Jul, :Aug, :Sep, :Oct, :Nov]
     bloommat = zeros(length(specpool), length(months))
@@ -55,22 +58,123 @@ function SpeciesData(tablefile::String, phylofile::String)
     )
 end
 
+function length(sd::SpeciesData)
+    return length(sd.specpool)
+end
 
+#= MixData =#
 @with_kw struct MixData
     nspecs::Int64
     totalweight::Float64
     specindices::Vector{Int64}
     specweights::Vector{Float64}
-    objectives::Vector{Float64}
+end
+
+function MixData(m::Vector{Tuple{Int64, Float64}})
+    nspecs = length(m)
+    si = [t[1] for t in m]
+    sw = [t[2] for t in m]
+    MixData(length(m), sum(sw), si, sw)
+end
+
+function length(md::MixData)
+    return md.nspecs
 end
 
 
-function evaluate!(mix::MixData, sd::SpeciesData)
-    pd = get_phylo_dist(mix, sd)
-    shannon = get_shannon(mix, sd)
-    cost = get_cost(mix, sd)
-    consval = get_consval(mix, sd)
-    bloom = get_bloom(mix, sd)
-    grass_spec_frac = get_grass_spec_frac(mix, sd)
-    grass_weight_frac = get_grass_weight_frac(mix, sd)
+
+#= Evaluation functions =#
+function get_phylo_dist(mix::MixData, sd::SpeciesData)
+    @unpack specindices = mix
+    @unpack phylo = sd
+    pd = 0.0
+    for s in specindices
+        for t in specindices
+            pd += phylo[s,t]
+        end
+    end
+    return pd /= 2.0
+end
+
+function get_shannon(mix::MixData, sd::SpeciesData)
+    @unpack specweights = mix
+    p = specweights ./ sum(specweights)
+    return shannon = exp(-1.0 * sum(p .* log.(p)))
+end
+
+function get_cost(mix::MixData, sd::SpeciesData)
+    @unpack specindices, specweights = mix
+    @unpack cost = sd
+    mixcost = 0.0
+    for (s, w) in zip(specindices, specweights)
+        mixcost += w * cost[s]
+    end
+    return mixcost
+end
+
+function get_consval(mix::MixData, sd::SpeciesData)
+    @unpack specindices, specweights = mix
+    @unpack conservatism = sd
+    mixcons = 0.0
+    for (s, w) in zip(specindices, specweights)
+        mixcons =+ w * conservatism[s]
+    end
+    return mixcons
+end
+
+function get_bloom(mix::MixData, sd::SpeciesData, penaltyfun=sqrt)
+    @unpack specindices, specweights = mix
+    @unpack bloom = sd
+
+    blooms_month_count = sum(bloom[specindices,:], dims=1)
+    return sum(penaltyfun.(blooms_month_count))
+end
+
+function get_grass_spec_frac(mix::MixData, sd::SpeciesData)
+    @unpack specindices = mix
+    @unpack grass = sd
+
+    mixG = Float64(sum(grass[specindices]))
+    return mixG/mix.nspecs
+end
+
+function get_grass_weight_frac(mix::MixData, sd::SpeciesData)
+    @unpack specindices, specweights = mix
+    @unpack grass = sd
+
+end
+
+#= GA helper functions =#
+
+function genome_to_mix(genome::Vector{Float64}, nspecs, mixweight)
+    p = Int64(length(genome)/2)
+    spec_priority = genome[1:p]
+    spec_weight = genome[(p+1):end]
+
+    spec_choice = sortperm(spec_priority, rev=true)[1:nspecs]
+    sw = spec_weight[spec_choice]
+
+    MixData(nspecs,
+            mixweight,
+            spec_choice,
+            sw .* (mixweight/sum(sw)))
+end
+
+function evaluate(objectivefuns::Vector{Function}, mix::MixData, sd::SpeciesData)
+    result = [f(mix, sd) for f in objectivefuns]
+    return result
+end
+
+function evaluate(objectivefuns::Vector{Function}, genome::Vector{Float64},
+                  sd::SpeciesData, nspecs::Int64, mixweight::Float64)
+    mix = genome_to_mix(genome, nspecs, mixweight)
+    result = [f(mix, sd) for f in objectivefuns]
+    return result
+end
+
+function random_mix(sd::SpeciesData, nspecs::Int64, weight::Float64)
+    specs = randperm(length(sd))[1:nspecs]
+    w = rand(nspecs)
+    w = w * (weight/sum(w))
+    return MixData(nspecs, weight, specs, w)
 end
